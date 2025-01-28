abfb071c5db4fdf8f9446e5b87890b31f5859ca8
minseoky <minseok184@gmail.com>
2025-01-29 00:09:58 +0900

    Init: 프로젝트 생성

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
+
+const App = () => {
+  return (
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+  );
+};
+
+export default App;
