885f715709e57442b845c02af8328bb331d14fce
minseoky <minseok184@gmail.com>
2025-02-01 23:27:40 +0900

    Feat: 글로벌 스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import GlobalStyles from "./GlobalStyles"; // 글로벌 스타일 추가
+import { AppContainer } from "./App.styles"; // App 스타일 가져오기
 
 const App = () => {
   return (
-      <div>
+    <>
+      <GlobalStyles />
+      <AppContainer>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </div>
+      </AppContainer>
+    </>
   );
 };
 
